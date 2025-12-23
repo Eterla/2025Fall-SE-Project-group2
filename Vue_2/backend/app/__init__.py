686d92a0573db4323538fb5dede6ce93651b38d2
pku2200094824 <enkhujinn@gmail.com>
2025-12-24 03:22:55 +0800

    Fix: 拼写错误

--- Vue_2/backend/app/__init__.py
+++ Vue_2/backend/app/__init__.py
@@ -39,10 +39,10 @@ def create_app(test_config=None):
         pass
 
     # 启用CORS
-    CORS(app, supports_credentials=True, origins="*", async_mode='eventlet')
+    CORS(app, supports_credentials=True, origins="*")
 
     # Init SocketIO（支持跨域）
-    socketio.init_app(app, cors_allowed_origins="*")
+    socketio.init_app(app, cors_allowed_origins="*", async_mode='eventlet')
     logger.info("SocketIO初始化完成")
 
     # 初始化数据库
